d23240e90c3ee8452b9e4cb38467a2459a5f6ea4
pratik Deo <pratikdeo78@gmail.com>
2020-11-11 00:08:38 -0500

    Implemented SignUp with user and server side validation

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-
+const data = require("../../data");
+const validators = data.validators;
 router.get("/", async function (req, res) {
   res.render("differentPages/landingPage", {
     title: "This is the landing Page",
@@ -12,11 +13,51 @@ router.get("/signup", async function (req, res) {
 });
 
 router.post("/signup", async function (req, res) {
-  let post = req.body;
-  try {
-    res.render("differentPages/landingPage");
-  } catch (e) {
-    console.log(e);
+  const errors = [];
+  const {
+    firstName,
+    lastName,
+    gender,
+    email,
+    password,
+    passwordConfirm,
+  } = req.body;
+  if (!validators.isLettersOnly(firstName))
+    errors.push("First name is missing");
+  if (!validators.isLettersOnly(lastName)) errors.push("Last name is missing");
+  if (!validators.isNonEmptyString(gender)) errors.push("Gender is missing");
+  else if (!validators.validateGender(gender))
+    errors.push("Gender provided is invalid");
+  if (!validators.isNonEmptyString(email))
+    errors.push("Email address is missing");
+  else if (!validators.isValidEmail(email))
+    errors.push("The provided emails is incorrect");
+  if (!validators.isNonEmptyString(password))
+    errors.push("Password is missing");
+  else if (!validators.isValidPassword(password))
+    errors.push("Password must be at least of length 8");
+  if (!validators.isNonEmptyString(passwordConfirm))
+    errors.push("Password confirmation is missing");
+  if (password !== passwordConfirm)
+    errors.push("Password and confirmation don't match");
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  let user = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email.toLowerCase(),
+    hashedPassword,
+    gender: gender,
+  };
+  if (errors.length > 0) {
+    return res.status(400).render("differentPages/Signup", { errors, user });
+  } else {
+    try {
+    } catch (e) {
+      return res
+        .status(500)
+        .render("differentPages/Signup", { errors: [e], user });
+    }
   }
 });
 
