fdbaa2ea58b2adbe45b61d826606e128c61e2a12
RohanRatwani <ratwani.rohan@gmail.com>
2020-11-15 15:47:20 -0500

    Synchronizing updates

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 //import different route modules
 const main_file = require("./main");
+const home_file = require("./home");
 
 const constructorMethod = (app) => {
   app.use("/", main_file);
+  app.use("/home", home_file);
 
   app.use("*", (req, res) => {
     res.status(404).json({ error: "Not found" });
