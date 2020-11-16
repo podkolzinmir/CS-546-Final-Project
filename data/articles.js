1c9fcd3f56506091d46b8f4eb550bc8b977ce948
jdyer21 <jdyer@stevens.edu>
2020-11-16 12:24:05 -0500

    added create and get functions for articles

---
+++ data/articles.js
@@ -0,0 +1,60 @@
+const axios = require('axios').default;
+mongoCollections = require("./../config/mongoCollection")
+const articles = mongoCollections.articles;
+const { ObjectId } = require('mongodb')
+
+async function create(url, keywords, wordCount, readTime, title, date){
+  if(url == null || keywords == null || wordCount == null || readTime == null || title == null || date == null){
+    throw "requires fields url, keywords, wordCount, readTime, title, date";
+  }if(typeof url != 'string' || typeof title != 'string' || typeof date != 'string' || url == '' || title == '' || date == ''){
+    throw "fields url, title, date must all be nonempty strings";
+  }if(typeof wordCount != 'number' || typeof readTime != 'number'){
+    throw "fields wordCount and readTime must be numbers.";
+  }if(!(Array.isArray(keywords)) || keywords.length == 0){
+    throw "keywords must be a nonempty array";
+  }
+  var keywordFail = true;
+  for(i=0;i<keywords.length;i++){
+    if(typeof keywords[i] == 'string' && keywords[i] != ''){
+      keywordFail = false;
+    }
+  }if(keywordFail == true){
+    throw "at least one element of keywords must be a nonempty string.";
+  }
+  const articlesCollection = await articles();
+  let newArticle = {
+    article_URL: url,
+    keywords: keywords,
+    number_of_words: wordCount,
+    read_time: readTime,
+    title: title,
+    date_published: date
+  }
+  const insertInfo = await articlesCollection.insertOne(newArticle);
+  if (insertInfo.insertedCount == 0){
+    throw 'Could not add article';
+  }
+  return await get(insertInfo.insertedId.toString());
+}
+
+async function get(id) {
+    if (id == null || typeof id != 'string'){
+      throw "id must be a string";
+    }
+    try{
+      const objId = new ObjectId(id);
+      const articlesCollection = await articles();
+      const article = await articlesCollection.findOne({ _id: objId });
+      if (article == null){
+        throw "article not found";
+      }
+      return article;
+  }catch(e){
+    throw "article not found";
+  }
+}
+
+module.exports = {
+  create: create,
+  get: get
+}
--- data/index.js
+++ data/index.js
@@ -6,6 +6,8 @@ const users = mongoCollections.users;
 usersData = require('./users');
 const keywords = mongoCollections.keywords;
 keywordsData = require('./keywords');
+const articles = mongoCollections.articles;
+articlesData = require('./articles');
 
 
 
@@ -15,6 +17,8 @@ async function main(){
     console.log(user1);
     const keyword1 = await keywordsData.create("news", ["url1", "url2", "url3"]);
     console.log(keyword1);
+    const article1 = await articlesData.create("https://techcrunch.com/2020/10/27/wyze-launches-version-3-of-its-20-security-camera/", ["tech", "science"], 800, 7, "Wyze launches version 3 of its $20 security camera", "11/1/2020");
+    console.log(article1);
   }catch(e){
     console.log (e);
   }
