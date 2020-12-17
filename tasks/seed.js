698e4d0b8071c06aedec6f04e49cb39873d87d3e
meayesha <30566794+meayesha@users.noreply.github.com>
2020-12-17 18:33:18 -0500

    Update seed.js

--- tasks/seed.js
+++ tasks/seed.js
@@ -9,14 +9,14 @@ const usersData = data.users;
 async function main() {
     const db = await dbConnection();
     await db.dropDatabase();
-    await usersData.create({firstname: "john", lastname: "doe"}, "abc@gmail.com",["Technology", "Art", "Science"],[], "what is your nickname", "alpha", "$123down");
-    await usersData.create({firstname: "claire", lastname: "ren"}, "claire@gmail.com",["Mucis", "Covid-19"], [],"what is your favourite colour", "red", "IceCream123#");
-    await usersData.create({firstname: "ayesha", lastname: "parveen"}, "aparveen@stevens.edu",["Technology", "Art", "Science","Music"], [],"what is your dog's name", "bella", "$1235Bella");
-    await usersData.create({firstname: "rohan", lastname: "ratwani"}, "ratwani.rohan@gmail.com",[],[], "what is your mother's maiden name", "seema", "$123Ronnie");
-    await usersData.create({firstname: "pratik", lastname: "deo"}, "pdeo1@stevens.edu",["Music"],[], "what is favourite sport", "cricket", "Tech12$3");
-    await usersData.create({firstname: "elizabeth", lastname: "timana"}, "eli@gmail.com",["Medicine","Politics"], [],"what is favourity place", "Miami", "Pain720#");
-    await usersData.create({firstname: "bella", lastname: "iqbal"}, "bella@gmail.com",[], [],"who is your favourite superhero", "batman", "Tan#432");
-    await usersData.create({firstname: "simon", lastname: "cobwell"}, "cobwell.simon@gmail.com",["Food"],[], "favourite animal", "cat", "#cheftan1234");
+    await usersData.create({firstname: "John", lastname: "Doe"}, "abc@gmail.com",["Technology", "Art", "Science"],[], "what is your nickname", "alpha", "$123down");
+    await usersData.create({firstname: "Claire", lastname: "Ren"}, "claire@gmail.com",["Mucis", "Covid-19"], [],"what is your favourite colour", "red", "IceCream123#");
+    await usersData.create({firstname: "Ayesha", lastname: "Parveen"}, "aparveen@stevens.edu",["Technology", "Art", "Science","Music"], [],"what is your dog's name", "bella", "$1235Bella");
+    await usersData.create({firstname: "Rohan", lastname: "Ratwani"}, "ratwani.rohan@gmail.com",[],[], "what is your mother's maiden name", "seema", "$123Ronnie");
+    await usersData.create({firstname: "Pratik", lastname: "Deo"}, "pdeo1@stevens.edu",["Music"],[], "what is favourite sport", "cricket", "Tech12$3");
+    await usersData.create({firstname: "Elizabeth", lastname: "Timana"}, "eli@gmail.com",["Medicine","Politics"], [],"what is favourity place", "Miami", "Pain720#");
+    await usersData.create({firstname: "Bella", lastname: "Iqbal"}, "bella@gmail.com",[], [],"who is your favourite superhero", "batman", "Tan#432");
+    await usersData.create({firstname: "Simon", lastname: "Cobwell"}, "cobwell.simon@gmail.com",["Food"],[], "favourite animal", "cat", "#cheftan1234");
     
     console.log('Done seeding database');
 ​
